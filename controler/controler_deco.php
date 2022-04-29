69f6f5b1af463f881b8cbe20533538b44977822e
Hafarou <hafaroupro@gmail.com>
2022-04-29 15:16:12 +0200

    Update fichiers

--- controler/controler_deco.php
+++ controler/controler_deco.php
@@ -1,6 +1,4 @@
 <?php
-    // ouverture de la session
-    session_start();
     // destruction de la session
     session_destroy();
     // on rediriige vers la page de connexio après 0s
--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
-    // ouverture de session
+    // ouverture de la session
     session_start();
 
     //Analyse de l'URL avec parse_url() et retourne ses composants
     $url = parse_url($_SERVER['REQUEST_URI']);
     //test soit l'url a une route sinon on renvoi à la racine
     $path = isset($url['path']) ? $url['path'] : '/';
-    
+
     /*------------------------------------------- ROUTER -------------------------------------------*/
     //test de la valeur $path dans l'URL et import de la ressource
     switch($path){
